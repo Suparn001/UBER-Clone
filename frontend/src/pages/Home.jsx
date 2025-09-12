c501c6c2a35da3d403bd2f0c9849141c12431f10
Suparn001 <suparngautam@gmail.com>
2025-09-12 15:10:41 +0530

    style: add rounded border to Continue button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <img className='w-16 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
             <div className='bg-white pb-8 py-4 px-4'>
                 <h2 className='text-[30px] font-semibold'>Get Started with Uber</h2>
-                <Link to="/login" className='flex items-center justify-center w-full bg-black text-white py-3 mt-5 rounded'>Continue</Link>
+                <Link to="/login" className='flex items-center justify-center w-full bg-black text-white py-3 mt-5 rounded-lg'>Continue</Link>
             </div>
         </div>
     )
